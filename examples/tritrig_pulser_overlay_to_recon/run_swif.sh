6498508f440f24be10e66f4eb6f407ad633d8fc7
Sarah Gaiser <s.gaiser96@gmx.de>
2023-11-21 11:16:53 -0800

    working example

---
+++ examples/tritrig_pulser_overlay_to_recon/run_swif.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hps-mc-batch swif -c /u/group/hps/users/caot/hps-mc/config/jlab_tongtong.cfg  -c .hpsmc -W 24 -l /farm_out/hps/caot/mc_production/2019/tritrig_pulser/logs signal_pulser_overlay_to_recon jobs.json
