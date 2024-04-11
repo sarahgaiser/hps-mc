ba5e6bf43387cfe3cdc5eb05276c9b2ee13f0927
Sarah Gaiser <sgaiser@stanford.edu>
2024-04-11 14:37:18 -0700

    saving status

--- prod/slac/ap_beam/recon_pulser/subArray.sh
+++ prod/slac/ap_beam/recon_pulser/subArray.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/scl enable devtoolset-8 -- /bin/bash
+#!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 #SBATCH --mem=1500M
-#SBATCH --array=19-20
-#SBATCH --partition=shared
+#SBATCH --array=1-20
+#SBATCH --account=HPS:hps-prod
 
 source $HPSMC/install/bin/hps-mc-env.sh
-export LD_LIBRARY_PATH=/sdf/group/hps/users/bravo/src/gsl-2.6/install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/fs/ddn/sdf/group/hps/users/bravo/src/gsl-2.6/install/lib:$LD_LIBRARY_PATH
 
 export FIRST_ID=0
 export JOB_ID=$(($SLURM_ARRAY_TASK_ID+$FIRST_ID))
@@ -19,4 +19,5 @@ cd $RUNDIR
 /bin/python3 $HPSMC_DIR/lib/python/hpsmc/job.py run -d $RUNDIR -c $JOBDIR/.hpsmc -i ${JOB_ID} readout_recon $JOBDIR/jobs.json > $RUNDIR/../logs/job.${JOB_ID}.log
 
 #SBATCH --output=/dev/null
+#SBATCH --partition=shared
 # HPSMC points to hps-mc directory. You might need to set this variable before running this script.
