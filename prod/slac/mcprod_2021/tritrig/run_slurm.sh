e8d79969263c608f178f14bf5df454feb327030a
Sarah Gaiser <sgaiser@stanford.edu>
2025-02-24 13:19:32 -0800

    adding rad and simp

--- prod/slac/mcprod_2021/tritrig/run_slurm.sh
+++ prod/slac/mcprod_2021/tritrig/run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export JOBDIR=/sdf/home/s/sgaiser/src/hps-mc/prod/slac/mcprod_2021/tritrig
-export RUNDIR=/fs/ddn/sdf/scratch/s/sgaiser/prod/tritrig/gen
+export RUNDIR=/sdf/data/hps/physics2021/run/prod/tritrig/gen
 
-hps-mc-batch slurm -o -r 5501:6000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -q milano -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l /sdf/data/hps/physics2021/mc/gen/tritrig/pass01/logs tritrig_gen_to_slic $JOBDIR/jobs.json 
+hps-mc-batch slurm -o -r 1:1000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -q milano -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l /sdf/data/hps/physics2021/mc/gen/tritrig/pass01/logs tritrig_gen_to_slic $JOBDIR/jobs.json 
 
