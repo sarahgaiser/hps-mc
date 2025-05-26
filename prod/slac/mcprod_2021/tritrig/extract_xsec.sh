77d97a715e6f3fad9aeb1179367c9c78154edeb1
Sarah Gaiser <sgaiser@stanford.edu>
2025-05-26 10:51:07 -0700

    adding tools and ap workspace

---
+++ prod/slac/mcprod_2021/tritrig/extract_xsec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+output_file="cross_sections_tritrig.txt"
+echo "xsec,nevents" > "$output_file"
+
+END=5000
+
+for i in $(seq 1 $END); do
+  echo $i
+  xsec=""
+  nevents=""
+
+  lhe_file=/sdf/data/hps/physics2021/mc/gen/tritrig/lhe/tritrig_3pt74_unweighted_events_$i.lhe.gz
+  nevents=$(zgrep "Number of Events" "$lhe_file" | awk -F ': ' '{print $2}' | awk '{printf "%.5E", $1}')
+  xsec=$(zgrep "Integrated weight (pb)" "$lhe_file" | awk -F ': ' '{print $2}' | awk '{printf "%.5E", $1}')
+
+  echo "$xsec,$nevents" >> "$output_file"
+done
