a424e32129db261f7da6422ef17b2fb4b8f10e26
Matthew Gignac <mgignac@slac.stanford.edu>
2026-02-04 10:14:15 -0800

    black python/jobs/data_cnv_job.py

--- python/jobs/data_cnv_job.py
+++ python/jobs/data_cnv_job.py
@@ -3,12 +3,13 @@
 
 Convert EVIO to LCIO and then process with HPSTR to produce a recon tuple.
 """
+
 from hpsmc.tools import EvioToLcio, HPSTR
 
-job.description = 'EVIO converter'
+job.description = "EVIO converter"
 
-cnv = EvioToLcio(steering='recon')
+cnv = EvioToLcio(steering="recon")
 
-#tuple = HPSTR(run_mode=1, cfg='recon')
+# tuple = HPSTR(run_mode=1, cfg='recon')
 
 job.add([cnv])
