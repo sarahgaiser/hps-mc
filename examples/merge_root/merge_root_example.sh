13bed9da20d5f457a779d7fa2165f7ae6bb4c59c
Matthew Gignac <mgignac@slac.stanford.edu>
2026-02-04 10:10:22 -0800

    example script general

--- examples/merge_root/merge_root_example.sh
+++ examples/merge_root/merge_root_example.sh
@@ -10,17 +10,15 @@
 #   -r: Run directory pattern (default: hps_*)
 #   -F: Path filter - only include files whose full path contains this string
 
-PARENT_DIR="/sdf/data/hps/physics2021/mc/hpstr/simp_pulser_new/pass5_v9/HPS_Run2021Pass1_v9_14272/"
-#PARENT_DIR="/sdf/data/hps/physics2021/mc/hpstr/ap_pulser/pass5_v9/"
-#PARENT_DIR="/sdf/data/hps/physics2021/mc/hpstr/tritrig_pulser/pass5_v9/HPS_Run2021Pass1_v9_14272_hitSmearKill-reprocess/"
+PARENT_DIR=""
 OUTPUT_PREFIX="merge_jobs"
-MAX_FILES=25
+MAX_FILES=50
 
 hps-mc-prepare-merge-jobs \
     $PARENT_DIR \
     -o $OUTPUT_PREFIX \
     -n $MAX_FILES \
-#    -F reprocess
+    -F reprocess
 #    -r ap* \
 #    -F "pass5_v9" \
 #    --single-list
@@ -38,8 +36,10 @@ for batch_file in ${OUTPUT_PREFIX}_batch*_files.txt; do
     hps-mc-job-template \
         -j $batch_num \
         -i root_files $batch_file $(wc -l < $batch_file) \
-        signal.tmpl \
+        merge_root.json.tmpl \
         ${OUTPUT_PREFIX}_batch${batch_num}_jobs.json
 done
+
+#signal.tmpl \
 
 cat ${OUTPUT_PREFIX}_batch*_jobs.json | jq -s 'add' > ${OUTPUT_PREFIX}_jobs.json
