ec8428206d836f6ef8ec1c5735a318b74c5b15e4
Sarah Gaiser <sgaiser@stanford.edu>
2026-04-02 09:05:04 -0700

    saving status and update to add_mother_full_truth

--- prod/slac/mcprod_2021/wab/overlay_to_hpstr/run_slurm.sh
+++ prod/slac/mcprod_2021/wab/overlay_to_hpstr/run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export JOBDIR=/sdf/home/s/sgaiser/src/hps-mc/prod/slac/mcprod_2021/wab/overlay_to_hpstr
-export RUNDIR=/sdf/data/hps/physics2021/run/prod/wab/overlay/14272
+export RUNDIR=/sdf/data/hps/physics2021/run/prod/wab/overlay/14596
 
-hps-mc-batch slurm -o -r 1:2000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -m 6000 -q milano -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l $RUNDIR/logs signal_pulser_overlay_to_recon $JOBDIR/jobs_14272.json 
+hps-mc-batch slurm -o -r 1000:2000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -m 6000 -q roma -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l $RUNDIR/logs signal_pulser_overlay_to_recon $JOBDIR/jobs_14596.json 
 
 #hps-mc-batch slurm -o -r 1:1000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 2 -m 6000 -q milano -A HPS:hps-prod -d $RUNDIR -c $JOBDIR/.hpsmc -l $RUNDIR/logs recon $JOBDIR/jobs_recon.json
