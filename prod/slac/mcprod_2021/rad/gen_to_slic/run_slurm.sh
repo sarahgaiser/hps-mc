93cf634c91bffb2eca68db25b76c7bad7201f211
Sarah Gaiser <sgaiser@stanford.edu>
2026-04-07 16:26:30 -0700

    changing to roma

--- prod/slac/mcprod_2021/rad/gen_to_slic/run_slurm.sh
+++ prod/slac/mcprod_2021/rad/gen_to_slic/run_slurm.sh
@@ -3,5 +3,7 @@
 export JOBDIR=/sdf/home/s/sgaiser/src/hps-mc/prod/slac/mcprod_2021/rad/gen_to_slic
 export RUNDIR=/sdf/data/hps/physics2021/run/prod/rad/gen
 
-hps-mc-batch slurm -o -r 1:2000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -q roma -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l /sdf/data/hps/physics2021/mc/gen/rad/pass5_v9/logs rad_gen_to_slic $JOBDIR/jobs.json 
+#hps-mc-batch slurm -o -r 1:20 -E $JOBDIR/setup.sh -W 3 -q roma -A HPS:hps-prod -d $RUNDIR -c $JOBDIR/.hpsmc -l /sdf/data/hps/physics2021/mc/gen/rad/pass5_v9/logs rad_gen_to_slic $JOBDIR/jobs.json 
+
+hps-mc-batch slurm -o -r 1:2000 -E /sdf/home/s/sgaiser/src/hps-mc/install/bin/hps-mc-env.sh -W 3 -q roma -A HPS:hps-prod -d $RUNDIR  -c $JOBDIR/.hpsmc -l /sdf/data/hps/physics2021/mc/gen/rad/pass5_v9/logs rad_gen_to_slic $JOBDIR/jobs.json
 
---
+++ prod/slac/mcprod_2021/rad/gen_to_slic/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export HPSMC_DIR=/sdf/home/s/sgaiser/src/hps-mc/install
+
+export PATH=$HPSMC_DIR/bin:$PATH
+
+#export LD_LIBRARY_PATH=$HPSMC_DIR/lib:$LD_LIBRARY_PATH
+source /sdf/home/s/sgaiser/run/gen/tritrig/tridents_for_ale/python3_venv/bin/activate
