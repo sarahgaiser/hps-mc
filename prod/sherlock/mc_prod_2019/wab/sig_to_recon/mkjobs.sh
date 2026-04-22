9b204ab2ff93bbb9f9e7513efb1f7ecb1d4175a6
mghrear <mghrear@stanford.edu>
2026-04-22 14:07:54 -0700

    adding 2019 wab prod files

---
+++ prod/sherlock/mc_prod_2019/wab/sig_to_recon/mkjobs.sh
@@ -0,0 +1,1 @@
+hps-mc-job-template -i wab wab_files.txt 8 -j 1 -m 124 -a vars.json job.json.tmpl jobs.json
---
+++ prod/sherlock/mc_prod_2019/wab/signal_pulser_overlay_to_recon/mkjobs.sh
@@ -0,0 +1,1 @@
+hps-mc-job-template -i wab wab_files.txt 8 -i pulser pulser_files.txt 1 -j 1 -m 124 -a vars.json job.json.tmpl jobs.json
