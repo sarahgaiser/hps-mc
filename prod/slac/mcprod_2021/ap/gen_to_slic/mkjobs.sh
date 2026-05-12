ec836e91f1946a041eb3351e66d7201599cda878
Sarah Gaiser <sgaiser@stanford.edu>
2026-05-12 10:02:28 -0700

    shifted target updates

--- prod/slac/mcprod_2021/ap/gen_to_slic/mkjobs.sh
+++ prod/slac/mcprod_2021/ap/gen_to_slic/mkjobs.sh
@@ -1,1 +1,1 @@
-hps-mc-job-template -j 1 -r 10000 -a vars.json job_post_gen.json.tmpl jobs_ap70.json
+hps-mc-job-template -j 1 -r 10000 -a vars_tgt_m0pt1mm.json job_post_gen.json.tmpl jobs_ap80_tgt_m0pt1mm.json
--- prod/slac/mcprod_2021/ap/overlay_to_hpstr/mkjobs.sh
+++ prod/slac/mcprod_2021/ap/overlay_to_hpstr/mkjobs.sh
@@ -1,3 +1,3 @@
 #hps-mc-job-template -i ap ap80_file_list.txt 4 -j 1 -m 500 -a vars.json job.json.tmpl jobs_ap80.json
 #hps-mc-job-template -i tritrig tritrig_files_v8_14272.txt 8 -i pulser pulser_files.txt 1 -j 1 -m 200 -a vars.json job_evio.json.tmpl jobs_14272.json
-hps-mc-job-template -j 1 -r 750 -a vars.json job.json.tmpl jobs_ap180.json
+hps-mc-job-template -j 1 -r 750 -a vars.json job_tgt_m0pt1mm.json.tmpl jobs_ap80_tgt_m0pt1mm.json
